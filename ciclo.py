27603fb8b2b3a5e51aecc2690d0934f27454aaab
Andy1126co <dangars@misena.edu.co>
2021-03-26 20:22:37 -0500

    ciclos

--- ciclo.py
+++ ciclo.py
@@ -10,3 +10,13 @@ print()
 diccionario = {'agua':100000,'gas':40000,'electricidad':60000,'internet':100000}
 for llave, valor in diccionario.items():
     print(llave,valor)
+
+pago = ['agua','internet','gas']
+pago_total = 0
+
+for llave, valor in diccionario.items():
+    if llave in pago:
+        pago_total = pago_total + valor
+print("voy a cancelar " , pago_total)
+
+
