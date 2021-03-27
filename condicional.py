8b845ee6763ccc15715628409f84ea6b9cf7dca3
Andy1126co <dangars@misena.edu.co>
2021-03-26 19:30:35 -0500

    Condicional

---
+++ condicional.py
@@ -0,0 +1,19 @@
+condicion_1= 0
+condicion_2 =1
+
+if(condicion_1 and condicion_2) or condicion_2:
+    print("Es verdad")
+else:
+    print("No es verdad")
+print()
+
+variable1= 1
+variable2= 0
+
+if (variable1 and variable2) or variable2:
+    print("Es verdad")
+elif variable1:
+    print("Solo se cumple la condicion 1")
+else:
+    print("Ninguna fue verdad es verdad")
+ 
